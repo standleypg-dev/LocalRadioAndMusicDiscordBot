3144c89aedaee15f3e76782273ec254fa1a3031a
standleypg.dev <standleypg.dev@gmail.com>
2024-07-21 10:17:50 +0000

    Revert "Merged PR 55: chore: Update archive file path in azure-pipelines.yml and optimize deploymen...
    chore: Update archive file path in azure-pipelines.yml and optimize deployment process"

--- Services/Implementations/AudioService.cs
+++ Services/Implementations/AudioService.cs
@@ -12,6 +12,7 @@ public class AudioService : IAudioService
 {
     private readonly YoutubeClient _youtubeClient;
     private IVoiceChannel _currentVoiceChannel;
+    private IAudioClient _audioClient;
     private bool isPlaying = false;
     private bool isRadioPlaying = false;
     private List<Song> songs = new();
@@ -23,8 +24,8 @@ public class AudioService : IAudioService
 
     public async Task InitiateVoiceChannelAsync(IVoiceChannel voiceChannel, string audioUrl, bool isYt = false)
     {
-        // if (_currentVoiceChannel != null)
-        //     await DestroyVoiceChannelAsync();
+        if (_currentVoiceChannel != null)
+            await DestroyVoiceChannelAsync();
         try
         {
             isPlaying = true;
@@ -44,15 +45,14 @@ public class AudioService : IAudioService
     private async Task ConnectToVoiceChannelAsync(IVoiceChannel voiceChannel, string audioUrl)
     {
         List<Task> tasks = new();
-        IAudioClient _audioClient = await voiceChannel.ConnectAsync();
+        var ffmpeg = CreateStream(audioUrl);
+        
+        var audioOutStream = ffmpeg.StandardOutput.BaseStream;
+        _audioClient = await voiceChannel.ConnectAsync();
         var discordStream = _audioClient.CreatePCMStream(AudioApplication.Music);
+
         //increase the buffer size to prevent the song ending early
         var bufferedStream = new BufferedStream(discordStream, 16348);
-        
-        var ffmpeg = CreateStream(audioUrl);
-
-        var audioOutStream = ffmpeg.StandardOutput.BaseStream;
-
 
         // Store the current voice channel
         SetBotCurrentVoiceChannel(voiceChannel);
@@ -64,8 +64,8 @@ public class AudioService : IAudioService
 
         if (songs.Count > 0)
             await NextSongAsync();
-        else
-            await DestroyVoiceChannelAsync();
+
+        await DestroyVoiceChannelAsync();
     }
 
     public async Task DestroyVoiceChannelAsync()
